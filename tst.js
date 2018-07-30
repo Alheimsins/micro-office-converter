508ac37810cd4961722220800c10974a0fbebcde
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-07-30 14:15:18 +0200

    Initial commit

--- tst.js
+++ tst.js
@@ -1,8 +1,17 @@
-const convert = require('./lib/convert')
+const createDirs = require('./lib/create-directories')
 
+/*
+const convert = require('./lib/convert')
 const options = {
   format: 'png',
   file: '../../../Downloads/myfile.pdf'
 }
 
 convert(options)
+*/
+async function mkdirs () {
+  const dirs = await createDirs()
+  console.log(dirs)
+}
+
+mkdirs()
